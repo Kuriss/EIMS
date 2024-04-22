833f7b65412e3222497216ea5ebdf0326bd84431
bood_y <2714481507@qq.com>
2024-04-22 16:11:07 +0800

    舍长大sb

--- src/main/java/com/eims/eims/EIMSApplication.java
+++ src/main/java/com/eims/eims/EIMSApplication.java
@@ -1,6 +1,7 @@
 package com.eims.eims;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
@@ -11,7 +12,7 @@ public class EIMSApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(EIMSApplication.class.getResource("MainWindow.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1200, 640);
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("电子图片管理系统");
         stage.setScene(scene);
         stage.show();
@@ -20,4 +21,6 @@ public class EIMSApplication extends Application {
     public static void main(String[] args) {
         launch();
     }
+
+
 }
--- src/main/java/com/eims/eims/EIMSController.java
+++ src/main/java/com/eims/eims/EIMSController.java
@@ -1,14 +1,6 @@
 package com.eims.eims;
 
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-
 public class EIMSController {
-    @FXML
-    private Label welcomeText;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to EIMS Application!");
-    }
+
 }
